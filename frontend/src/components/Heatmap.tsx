4094d3f475744f79b1895972be43cb66937c0af7
rvigee <contact@raphaelvigee.com>
2020-06-06 14:46:02 +0200

    Add generated icons

--- frontend/src/components/Heatmap.tsx
+++ frontend/src/components/Heatmap.tsx
@@ -4,7 +4,7 @@ I promise I will clean this up.
 */
 
 import * as React from 'react';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import useStations, { Station } from '../hooks/withStations';
 import ReactMapboxGl, { Layer, Source } from 'react-mapbox-gl';
 import * as mapboxgl from 'mapbox-gl';
@@ -88,6 +88,120 @@ function findBikesType<S extends DataFileStation, P extends 'mechanical' | 'ebik
     return 0;
 }
 
+interface DrawPiechartProps {
+    canvas: HTMLCanvasElement;
+    data: number[];
+    colors: string[];
+    doughnutHoleSize: number;
+}
+
+function drawPiechart({ canvas, data, doughnutHoleSize, colors }: DrawPiechartProps) {
+    const ctx = canvas.getContext('2d')!;
+
+    let total_value = 0;
+    let color_index = 0;
+    for (const val of data) {
+        total_value += val;
+    }
+
+    function drawPieSlice(centerX, centerY, radius, startAngle, endAngle, color) {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(centerX, centerY);
+        ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    let start_angle = 0;
+    for (const val of data) {
+        const slice_angle = (2 * Math.PI * val) / total_value;
+
+        drawPieSlice(
+            canvas.width / 2,
+            canvas.height / 2,
+            Math.min(canvas.width / 2, canvas.height / 2),
+            start_angle,
+            start_angle + slice_angle,
+            colors[color_index % colors.length],
+        );
+
+        start_angle += slice_angle;
+        color_index++;
+    }
+
+    if (doughnutHoleSize) {
+        drawPieSlice(
+            canvas.width / 2,
+            canvas.height / 2,
+            doughnutHoleSize * Math.min(canvas.width / 2, canvas.height / 2),
+            0,
+            2 * Math.PI,
+            '#fff',
+        );
+    }
+}
+
+interface DrawUnavailableProps {
+    canvas: HTMLCanvasElement;
+    fill?: string;
+    stroke?: {
+        width: number;
+        color: string;
+    };
+}
+
+function drawCircle({ canvas, fill, stroke }: DrawUnavailableProps) {
+    const ctx = canvas.getContext('2d')!;
+
+    ctx.beginPath();
+    let radius = Math.min(canvas.width / 2, canvas.height / 2);
+
+    if (stroke) {
+        radius -= stroke.width;
+    }
+
+    ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+    ctx.closePath();
+
+    if (stroke) {
+        ctx.lineWidth = stroke.width;
+        ctx.strokeStyle = stroke.color;
+        ctx.stroke();
+    }
+
+    if (fill) {
+        ctx.fillStyle = fill;
+        ctx.fill();
+    }
+}
+
+interface DrawTextProps {
+    canvas: HTMLCanvasElement;
+    text: string;
+    color: string;
+}
+
+function drawText({ canvas, text, color }: DrawTextProps) {
+    const ctx = canvas.getContext('2d')!;
+
+    ctx.fillStyle = color;
+    ctx.font = '300 11px Arial';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+
+    ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+}
+
+function fromEntries<T>(iterable: Array<[string, T]>): { [key: string]: T } {
+    return [...iterable].reduce<{ [key: string]: T }>((obj, [key, val]) => {
+        obj[key] = val;
+        return obj;
+    }, {});
+}
+
+const colors = { mechanical: '#80c664', ebike: '#16a2a8', dock: '#d468c9' };
+
 function Map({ file, showStations, numericalProp }: MapProps) {
     const [stations, setStations] = useState<EnhancedDataFileStation[]>([]);
     const { findStation } = useStations();
@@ -109,39 +223,59 @@ function Map({ file, showStations, numericalProp }: MapProps) {
         handleStations();
     }, [file]);
 
+    const mappedFeatures = useMemo(() => {
+        const pairs = stations
+            .filter((s) => !!s.station)
+            .map((s) => {
+                const stationData = s.station!;
+
+                const feature = {
+                    type: 'Feature',
+                    properties: {
+                        station_id: s.station_id,
+                        station_name: stationData.name,
+
+                        bikes_available: s.num_bikes_available,
+                        bikes_available_mechanical: findBikesType(s, 'mechanical'),
+                        bikes_available_ebike: findBikesType(s, 'ebike'),
+                        docks_available: s.num_docks_available,
+                        is_installed: s.is_installed,
+
+                        is_functional: s.is_installed && s.is_renting && s.is_returning,
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [stationData.lon, stationData.lat, 0],
+                    },
+                };
+
+                return [`${s.station_id}`, feature] as [string, typeof feature];
+            });
+
+        return fromEntries(pairs);
+    }, [stations]);
+
     const geojson = useMemo(
         () => ({
             type: 'FeatureCollection',
-            features: stations
-                .filter((s) => !!s.station)
-                .map((s) => {
-                    const stationData = s.station!;
-
-                    return {
-                        type: 'Feature',
-                        properties: {
-                            station_id: s.station_id,
-                            station_name: stationData.name,
-
-                            bikes_available: s.num_bikes_available,
-                            bikes_available_mechanical: findBikesType(s, 'mechanical'),
-                            bikes_available_ebike: findBikesType(s, 'ebike'),
-                            docks_available: s.num_docks_available,
-                        },
-                        geometry: {
-                            type: 'Point',
-                            coordinates: [stationData.lon, stationData.lat, 0],
-                        },
-                    };
-                }),
+            features: Object.values(mappedFeatures),
         }),
-        [stations],
+        [mappedFeatures],
     );
 
     const max = useMemo(() => Math.max(...geojson.features.map((f) => f.properties[numericalProp])), [
-        stations,
+        geojson,
         numericalProp,
     ]);
+
+    const store = useRef({
+        mappedFeatures,
+    });
+    useEffect(() => {
+        store.current = {
+            mappedFeatures,
+        };
+    }, [mappedFeatures]);
 
     const center = useMemo(() => [2.3488, 48.8534] as [number, number], []);
 
@@ -159,6 +293,84 @@ function Map({ file, showStations, numericalProp }: MapProps) {
                         trackUserLocation: true,
                     }),
                 );
+
+                const canvas = document.createElement('canvas');
+                canvas.width = 25;
+                canvas.height = 25;
+
+                map.on('styleimagemissing', (e) => {
+                    const id = e.id; // id of the missing image
+
+                    const [genId, ...dataStr] = id.split('::');
+
+                    if (genId === 'stationv1') {
+                        const [stationId, numProp]: [string, NumericalPropType] = dataStr;
+                        const feature = store.current.mappedFeatures[stationId];
+
+                        if (!feature) {
+                            return;
+                        }
+
+                        const props = feature.properties;
+
+                        if (props.is_functional) {
+                            switch (numProp) {
+                                case 'bikes_available':
+                                    drawPiechart({
+                                        canvas,
+                                        data: [
+                                            props.bikes_available_mechanical,
+                                            props.bikes_available_ebike,
+                                            props.docks_available,
+                                        ],
+                                        colors: [colors.mechanical, colors.ebike, colors.dock],
+                                        doughnutHoleSize: 0.7,
+                                    });
+                                    break;
+                                case 'bikes_available_mechanical':
+                                    drawCircle({
+                                        canvas,
+                                        fill: colors.mechanical,
+                                    });
+                                    break;
+                                case 'bikes_available_ebike':
+                                    drawCircle({ canvas, fill: colors.ebike });
+                                    break;
+                                case 'docks_available':
+                                    drawCircle({ canvas, fill: colors.dock });
+                                    break;
+                            }
+                        } else {
+                            drawCircle({ canvas, fill: '#fc6262' });
+                        }
+
+                        const getTextColor = () => {
+                            if (props.is_functional) {
+                                switch (numProp) {
+                                    case 'bikes_available':
+                                        return '#000';
+                                    default:
+                                        return '#fff';
+                                }
+                            } else {
+                                return '#fff';
+                            }
+                        };
+
+                        drawText({
+                            canvas,
+                            text: `${props[numProp]}`,
+                            color: getTextColor(),
+                        });
+
+                        const ctx = canvas.getContext('2d')!;
+                        const im = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                        map.addImage(id, im);
+                        return;
+                    }
+                });
             }}
         >
             <Source id={'stations'} geoJsonSource={{ type: 'geojson', data: geojson }} />
@@ -202,17 +414,10 @@ function Map({ file, showStations, numericalProp }: MapProps) {
                     id={'stations-locations'}
                     sourceId={'stations'}
                     layout={{
-                        'text-field': ['get', numericalProp],
-                        'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
-                        'text-letter-spacing': 0.05,
-                        'text-size': 11,
                         'symbol-sort-key': ['interpolate', ['linear'], ['get', numericalProp], 0, max, max, 0],
+                        'icon-image': `stationv1::{station_id}::${numericalProp}`,
                     }}
-                    paint={{
-                        'text-color': '#fff',
-                        'text-halo-color': '#4e4e4e',
-                        'text-halo-width': 1,
-                    }}
+                    paint={{}}
                 />
             ) : (
                 <></>
