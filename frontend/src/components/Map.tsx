bc108401508fbd04ad50a70ff132890b3501622d
rvigee <contact@raphaelvigee.com>
2020-06-14 00:12:17 +0200

    Make design a bit less crappy

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -5,6 +5,7 @@ import styles from './Map.scss?module';
 import Heatmap, { NumericalPropType } from './Heatmap';
 import { useDataFiles } from '../hooks/useDataFiles';
 import { DateTime } from 'luxon';
+import cx from 'classnames';
 
 export default function Map() {
     const files = useDataFiles();
@@ -24,38 +25,43 @@ export default function Map() {
     const currentFile = files[index];
 
     function filterButton(prop: NumericalPropType, name: string) {
+        const active = numericalProp === prop;
+
         return (
-            <button onClick={() => setNumProp(prop)}>
-                {numericalProp === prop && '* '} {name}
+            <button className={cx(styles.filter, { [styles.active]: active })} onClick={() => setNumProp(prop)}>
+                {name}
             </button>
         );
     }
 
     return (
         <div className={styles.view}>
-            <header className={styles.header}>
-                <button disabled={index === 0} onClick={() => setIndex(index - 1)}>
+            <div className={styles.topControls}>
+                <button className={styles.button} disabled={index === 0} onClick={() => setIndex(index - 1)}>
                     Previous
                 </button>
-                <span>
-                    {currentFile.live && <span style={{ color: 'red' }}>*</span>}({index}){' '}
-                    {currentFile.date.toLocaleString(DateTime.DATETIME_MED)}
-                </span>
-                <button disabled={index === maxIndex} onClick={() => setIndex(index + 1)}>
+                <div className={styles.currentDate}>
+                    <span>
+                        {currentFile.live && <span style={{ color: 'red' }}>&bull;</span>}({index}){' '}
+                        {currentFile.date.toLocaleString(DateTime.DATETIME_MED)}
+                    </span>
+                </div>
+                <button className={styles.button} disabled={index === maxIndex} onClick={() => setIndex(index + 1)}>
                     Next
                 </button>
-                <button onClick={() => setShowStations(!showStations)}>
-                    {showStations ? 'Show Heatmap' : 'Show stations'}
-                </button>
-            </header>
-            <header className={styles.header}>
+            </div>
+
+            <Heatmap file={currentFile} showStations={showStations} numericalProp={numericalProp} />
+
+            <div className={styles.filters}>
                 {filterButton('bikes_available', 'All bikes')}
                 {filterButton('bikes_available_mechanical', 'Mechanical')}
                 {filterButton('bikes_available_ebike', 'Ebike')}
                 {filterButton('docks_available', 'Docks')}
-            </header>
-
-            <Heatmap file={currentFile} showStations={showStations} numericalProp={numericalProp} />
+                <button className={styles.button} onClick={() => setShowStations(!showStations)}>
+                    {showStations ? 'Show Heatmap' : 'Show stations'}
+                </button>
+            </div>
         </div>
     );
 }
